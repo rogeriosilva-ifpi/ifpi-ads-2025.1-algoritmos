bf9c2aab3d9d525bccced5448b02eca44952edf7
Rogério Silva <rogerio410@192.168.0.12>
2025-03-26 12:02:50 -0300

    Primeiro Programa

---
+++ somatorio.py
@@ -0,0 +1,14 @@
+print('Hello ADS 2025.1')
+print('--------------------')
+print('>>>>> Soma de 1 até N <<<<')
+
+# entrada
+limite = int(input('Limite Superior: '))
+
+# processamento
+soma_par = limite + 1
+quantidade_pares = limite / 2
+somatorio = soma_par * quantidade_pares
+
+# saída
+print(somatorio)
